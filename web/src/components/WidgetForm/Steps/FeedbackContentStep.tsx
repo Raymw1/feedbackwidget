6409b8e4a60a34361753eef9ec94beda893b58d8
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-15 19:10:15 -0300

    Web: add textarea form on feedbackContentType component

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -27,8 +27,12 @@ export function FeedbackContentStep({ feedbackType, handleRestartFeedback }: Fee
         </span>
         <CloseButton />
       </header>
-      <div className='flex py-8 gap-2 w-full'>
-      </div>
+      <form className='my-4 w-full'>
+        <textarea
+          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:outline-none focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
+          placeholder='Please, tell us what is happening with details...'
+        />
+      </form>
     </>
   );
 }
