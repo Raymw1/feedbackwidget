29e06f0e7c162ea2fa2d2f324e2be3e9685f1352
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-15 19:22:09 -0300

    Web: add send feedback button on form

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -32,6 +32,14 @@ export function FeedbackContentStep({ feedbackType, handleRestartFeedback }: Fee
           className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:outline-none focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
           placeholder='Please, tell us what is happening with details...'
         />
+        <footer className='flex gap-2 mt-2'>
+          <button
+            type='submit'
+            className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
+          >
+            Send feedback
+          </button>
+        </footer>
       </form>
     </>
   );
