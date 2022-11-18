1aed6a32b207819fdc0e00f003b9c4e351f21ca4
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-18 19:59:35 -0300

    Server: Add sendMail function

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,33 @@
+import { Router } from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
-import { Router, Request, Response } from 'express';
 
-const routes = Router();
+export const routes = Router();
 
-routes.post('/feedbacks', async (request: Request, response: Response) => {
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "94562f537c158a",
+    pass: "af008be3643d8b"
+  }
+});
+
+routes.post('/feedbacks', async (request, response) => {
   const { type, comment, screenshot } = request.body;
   const feedback = await prisma.feedback.create({
     data: { type, comment, screenshot }
   });
+  await transport.sendMail({
+    from: 'Feedget Team <hey@feedget.com>',
+    to: 'Rayan Wilbert <ray@gmail.com>',
+    subject: 'New feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;"></div>`,
+      `<p>Feedback type: ${type}</p>`,
+      `<p>Comment: ${comment}</p>`,
+    ].join('\n')
+  });
   return response.status(201).json({ data: feedback });
 });
 
-export { routes };
