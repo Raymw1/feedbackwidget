d2cb6fe05a30ca0c643ae4fb86894c37d21a2874
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-18 20:21:37 -0300

    Server: applying dependecy inversion in submit feedback

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import nodemailer from 'nodemailer';
-import { prisma } from './prisma';
+
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = Router();
 
@@ -15,19 +17,23 @@ const transport = nodemailer.createTransport({
 
 routes.post('/feedbacks', async (request, response) => {
   const { type, comment, screenshot } = request.body;
-  const feedback = await prisma.feedback.create({
-    data: { type, comment, screenshot }
-  });
-  await transport.sendMail({
-    from: 'Feedget Team <hey@feedget.com>',
-    to: 'Rayan Wilbert <ray@gmail.com>',
-    subject: 'New feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;"></div>`,
-      `<p>Feedback type: ${type}</p>`,
-      `<p>Comment: ${comment}</p>`,
-    ].join('\n')
-  });
-  return response.status(201).json({ data: feedback });
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository);
+
+  await submitFeedbackUseCase.execute({ type, comment, screenshot });
+
+
+  // await transport.sendMail({
+  //   from: 'Feedget Team <hey@feedget.com>',
+  //   to: 'Rayan Wilbert <ray@gmail.com>',
+  //   subject: 'New feedback',
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111;"></div>`,
+  //     `<p>Feedback type: ${type}</p>`,
+  //     `<p>Comment: ${comment}</p>`,
+  //   ].join('\n')
+  // });
+  return response.status(201).json();
 });
 
