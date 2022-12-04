e4eb1c2534646aeb8ca8bf0f1b817ce0b93ebe86
Rayan <rayan.rmw@gmail.com>
2022-12-04 17:20:00 -0300

    Mobile: add custom fonts and app splash screen lib

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+
 import { Widget } from './src/components/Widget';
+
 import { theme } from './src/theme';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_500Medium });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]); 
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+    <View style={{ flex: 1, backgroundColor: theme.colors.background }} onLayout={onLayoutRootView}>
       <Widget />
       <StatusBar style="light" backgroundColor='transparent' translucent />
     </View>
